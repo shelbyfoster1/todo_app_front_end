3ee7cf50bb0aba8c6e94f77c152fdcc64682b252
Shelby Foster <sefoster2@cps.edu>
2018-04-20 20:02:22 +0000

    Install postgres and update database.yml v

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,31 +1,43 @@
 class TodoController < ApplicationController
     def index
+    @todos = Todo.all
     end
-    def show 
+    
+    def show
     @todo = Todo.find_by_id(params[:id])
-  end
-  def new
-end
-def create
+    end
+    
+    def new
+    end
+    
+    def create
   t = Todo.new
   t.description = params['description']
   t.pomodoro_estimate = params['pomodoro_estimate']
   t.save
   redirect_to "/todo/show/#{ t.id }"
-end
-def edit
+    end
+    
+    def edit
   @todo = Todo.find_by_id(params[:id])
-end
-def update
+    end
+    
+    def update
   t = Todo.find_by_id(params['id'])
   t.description = params['description']
   t.pomodoro_estimate = params['pomodoro_estimate']
   t.save
   redirect_to "/todo/show/#{t.id}"
-end
-def destroy
+    end
+    
+    def destroy
   t = Todo.find_by_id(params[:id])
   t.destroy
-  redirect_to "/todo/index"
-end
+  redirect_to "/"
+    end
+ 
+
+
+
+
 end
